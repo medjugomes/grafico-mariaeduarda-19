4605864b5cff75166f61e3c8a5b4470e80976ded
medjugomes <136620326+medjugomes@users.noreply.github.com>
2024-10-23 08:42:17 -0300

    Add files via upload

---
+++ grafico-mariaeduarda-19-main/README.md
@@ -0,0 +1,1 @@
+# grafico-mariaeduarda-19
---
+++ grafico-mariaeduarda-19-main/script.js
@@ -0,0 +1,19 @@
+var ctx = document.getElementById("myChart").getContext("2dc")
+var myChart = new Chart(ctx,{
+type:"pie",
+date:{
+    labels:["Instagram"
+ "WhatsApp",
+ "Facebook",
+ "WeChat",
+ "TikTok",
+ "DouYin",
+ "Twitter",
+ "Telegram",
+ "FB Messenger",
+ "LINE",
+]
+
+}
+}
+)
